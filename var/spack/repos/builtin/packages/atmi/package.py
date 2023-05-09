c7dca59c9de6c72bdcf7b773a9508b4434a66209
sreenivasaMurthy Kolam <sreenivasamurthy.kolam@amd.com>
2023-05-09 04:24:24 -0700

    initial commit for 5.5.0 rocm release

--- var/spack/repos/builtin/packages/atmi/package.py
+++ var/spack/repos/builtin/packages/atmi/package.py
@@ -15,11 +15,12 @@ class Atmi(CMakePackage):
 
     homepage = "https://github.com/RadeonOpenCompute/atmi"
     git = "https://github.com/RadeonOpenCompute/atmi.git"
-    url = "https://github.com/RadeonOpenCompute/atmi/archive/rocm-5.4.3.tar.gz"
+    url = "https://github.com/RadeonOpenCompute/atmi/archive/rocm-5.5.0.tar.gz"
     tags = ["rocm"]
 
     maintainers("srekolam", "renjithravindrankannath")
 
+    version("5.5.0", sha256="2238701c4d95bd984c2b9583ec738fd6b03b84a460f2a1ce18e591264f142485")
     version("5.4.3", sha256="243aae6614e5bd136a099102957a6d65a01434b620291349613ad63701868ef8")
     version("5.4.0", sha256="b5cce10d7099fecbb40a0d9c2f29a7675315471fe145212b375e37e4c8ba5618")
     version("5.3.3", sha256="cc1144e4939cea2944f6c72a21406b9dc5b56d933696494074c280df7469834a")
@@ -134,6 +135,7 @@ class Atmi(CMakePackage):
         "5.3.3",
         "5.4.0",
         "5.4.3",
+        "5.5.0",
     ]:
         depends_on("comgr@" + ver, type="link", when="@" + ver)
         depends_on("hsa-rocr-dev@" + ver, type="link", when="@" + ver)
